fdce987fb6f1909e2f7e2ca9b5effc130106b51b
thanadunbadboy <thanadunbadboy@gmail.com>
2025-05-17 17:45:24 +0700

    fix prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  // @ts-ignore
+  if (!global.prisma) {
+    // สร้าง client ใหม่ใน dev mode ถ้ายังไม่มี
+    global.prisma = new PrismaClient()
+  }
+  // ใช้ตัวเดิมถ้ามีแล้ว
+  prisma = global.prisma
 }
 
-export const prisma = globalThis.prisma ?? new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+export { prisma }
